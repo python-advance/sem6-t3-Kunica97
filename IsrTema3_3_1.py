b992e5d19928bd59414aef94ca6a4403908b6d00
Sofiya Kunitsyna <31761683+Kunica97@users.noreply.github.com>
2019-06-26 13:08:01 +0300

    Add files via upload

--- IsrTema3_3_1.py
+++ IsrTema3_3_1.py
@@ -2,41 +2,38 @@ from urllib.request import urlopen
 from xml.etree import ElementTree as ET
 
 class CurrencyBoard:
-    __instance = None
+    __instance = None # создаём переменную, в которую
+
+    @staticmethod
+    def getInstance():
+        if CurrencyBoard.__instance == None: # проверяем на наличие данных
+            CurrencyBoard()
+        return CurrencyBoard.__instance
+
     def __init__(self):
-        if not CurrencyBoard.__instance:
-            print('Делаем запрос')
-        else:
-            print('Запрос уже был сделан:', self.update())
-    @classmethod
-    def update(cls):
-        if not cls.__instance:
-            currencies_ids_lst = ['R01239', 'R01235', 'R01035', 'R01010', 'R01090',
-                                  'R01335', 'R01350', 'R01535', 'R01625', 'R01700']
-
-            cur_res_str = urlopen("http://www.cbr.ru/scripts/XML_daily.asp")
-
-            result = {}
-
-            cur_res_xml = ET.parse(cur_res_str)
-
-            root = cur_res_xml.getroot()
-            valutes = root.findall('Valute')
+        if CurrencyBoard.__instance == None:
+            currencies_ids_lst = ['R01239', 'R01235', 'R01035', 'R01010', 'R01090', # список id валют
+                                  'R01335', 'R01350', 'R01535', 'R01625', 'R01700'] # которые мы сами выбрали
+            cur_res_str = urlopen("http://www.cbr.ru/scripts/XML_daily.asp") # получаем данные с сервера
+            result = {} # создаём словарь
+            cur_res_xml = ET.parse(cur_res_str) # парсим полученные данные
+            root = cur_res_xml.getroot() # находим корневой элемент
+            valutes = root.findall('Valute') # находим элементы с соответствующим тегом
             for el in valutes:
-                valute_id = el.get('ID')
+                valute_id = el.get('ID') # получаем все id
+                if str(valute_id) in currencies_ids_lst: # находим наши id в списке всех id
+                    valute_cur_val = el.find('Value').text # записываем значение курса валют
+                    valute_cur_n = el.find('Name').text # записываем названия соответствующих курсов валют
+                    result[valute_id] = valute_cur_n, valute_cur_val # записываем полученные данные в словарь
+            CurrencyBoard.__instance = result # присваиваем переменной __instance значение словаря
 
-                if str(valute_id) in currencies_ids_lst:
-                    valute_cur_val = el.find('Value').text
-                    valute_cur_n = el.find('Name').text
-                    result[valute_id] = valute_cur_n, valute_cur_val
-            cls.__instance = result
-        return cls.__instance
+a = CurrencyBoard()
+print(a)
 
-# инициализируем класс и выводим значения, чтобы выводились только
-# наименования валют и текущий курс, используем .value(), так как получаем словарь
+b = CurrencyBoard.getInstance()
+print (b)
 
-print(CurrencyBoard.update().values())
-
-# проверяем, посылается ли запрос второй раз. В случае успеха,
-# консоль выведет "Запрос уже был сделан", с демонстрацией хранимых данных
-CurrencyBoard()
+c = CurrencyBoard.getInstance()
+print (c)
+assert b == c # сверяем значения адресов переменных
+print(id(b), id(c))
